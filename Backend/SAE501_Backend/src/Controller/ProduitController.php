a9bc3944cf04101012b8f87dba0518251834319c
NicoMarly <nicolasmarly02@gmail.com>
2024-12-09 17:39:18 +0100

    Ajout produit controller

---
+++ Backend/SAE501_Backend/src/Controller/ProduitController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Entity\Image;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/produit/post', name: 'create_produit', methods: ['POST'])]
+    public function CreateProduit(Request $request, EntityManagerInterface $em): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $produit = new Produit();
+
+        $produit->setNom($data['name']);
+        $produit->setDescription($data['content']);
+
+        $image = $em->getRepository(Image::class)->find($data['imageId']);
+        if ($image) {
+            $produit->setImage($image);
+        }
+
+        $em->persist($produit);
+        $em->flush();
+        return $this->json(['message' => 'Produit créé avec succès'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/produit/get', name: 'get_produits', methods: ['GET'])]
+    public function GetProduit(Request $request, EntityManagerInterface $em): Response
+    {
+        $produits = $em->getRepository(Produit::class)->findAll();
+        if ($produits) {
+            $data = array_map(function ($produit) {
+                if ($produit->getImage()) {
+                    $image = $produit->getImage();
+                    $path = $image->getPath();
+                } else {
+                    $path = null;
+                }
+                return [
+                    'id' => $produit->getId(),
+                    'name' => $produit->getNom(),
+                    'content' => $produit->getDescription(),
+                    'image' => $path
+                ];
+            }, $produits);
+            return $this->json($data, Response::HTTP_OK);
+        } else {
+            return $this->json(['message' => 'Pas de produits trouvés'], Response::HTTP_OK);
+        }
+    }
+
+    #[Route('/produit/{id}/get', name: 'get_produit_by_id', methods: ['GET'])]
+    public function GetProduitById(Request $request, EntityManagerInterface $em, $id): Response
+    {
+        $produits = $em->getRepository(Produit::class)->find($id);
+        if ($produits) {
+
+            if ($produits->getImage()) {
+                $image = $produits->getImage();
+                $imageId = $image->getId();
+                $path = $image->getPath();
+            } else {
+                $path = null;
+            }
+            $data = [
+                'id' => $produits->getId(),
+                'name' => $produits->getNom(),
+                'content' => $produits->getDescription(),
+                'imageId' => $imageId,
+                'image' => $path
+            ];
+
+            return $this->json($data, Response::HTTP_OK);
+        } else {
+            return $this->json(['message' => 'Pas de produits trouvés'], Response::HTTP_OK);
+        }
+    }
+
+    #[Route('/produit/{id}/put', name: 'put_produit_by_id', methods: ['PUT'])]
+    public function PutProduitById(Request $request, EntityManagerInterface $em, $id): Response
+    {
+        $produit = $em->getRepository(Produit::class)->find($id);
+
+        if ($produit) {
+            $data = json_decode($request->getContent(), true);
+
+            if (isset($data['name'])) {
+                $produit->setNom($data['name']);
+            } else {
+                return $this->json(['message' => 'Le champ "name" est requis'], Response::HTTP_BAD_REQUEST);
+            }
+
+            if (isset($data['content'])) {
+                $produit->setDescription($data['content']);
+            } else {
+                return $this->json(['message' => 'Le champ "content" est requis'], Response::HTTP_BAD_REQUEST);
+            }
+
+            if (isset($data['image'])) {
+                $image = $em->getRepository(Image::class)->find($data['image']);
+                if ($image) {
+                    $produit->setImage($image);
+                }
+            } else {
+                return $this->json(['message' => 'Le champ "image" est requis'], Response::HTTP_BAD_REQUEST);
+            }
+
+            $em->persist($produit);
+            $em->flush();
+
+            return $this->json(['message' => 'Produit mis à jour avec succès'], Response::HTTP_OK);
+        }
+
+        return $this->json(['message' => 'Pas de produit trouvé'], Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/produit/{id}/delete', name: 'delete_categorie_by_id', methods: ['DELETE'])]
+    public function DeleteProduitById(EntityManagerInterface $em, $id): Response
+    {
+        $categorie = $em->getRepository(Produit::class)->find($id);
+
+        if ($categorie) {
+            $em->remove($categorie);
+            $em->flush();
+
+            return $this->json(['message' => 'Produit supprimé avec succès'], Response::HTTP_OK);
+        }
+
+        return $this->json(['message' => 'Pas de produit trouvée'], Response::HTTP_NOT_FOUND);
+    }
+}
---
+++ Backend/SAE501_Backend/src/Entity/Produit.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Description = null;
+
+    #[ORM\Column]
+    private ?float $Prix = null;
+
+    /**
+     * @var Collection<int, Categorie>
+     */
+    #[ORM\ManyToMany(targetEntity: Categorie::class, inversedBy: 'produits')]
+    private Collection $Categorie;
+
+    #[ORM\ManyToOne(inversedBy: 'produits')]
+    private ?Image $Image = null;
+
+    public function __construct()
+    {
+        $this->Categorie = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(float $Prix): static
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Categorie>
+     */
+    public function getCategorie(): Collection
+    {
+        return $this->Categorie;
+    }
+
+    public function addCategorie(Categorie $categorie): static
+    {
+        if (!$this->Categorie->contains($categorie)) {
+            $this->Categorie->add($categorie);
+        }
+
+        return $this;
+    }
+
+    public function removeCategorie(Categorie $categorie): static
+    {
+        $this->Categorie->removeElement($categorie);
+
+        return $this;
+    }
+
+    public function getImage(): ?Image
+    {
+        return $this->Image;
+    }
+
+    public function setImage(?Image $Image): static
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+}
